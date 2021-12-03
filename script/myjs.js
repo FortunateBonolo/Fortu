62309be3a98b4a18d95fb5cbf68a4f080dda06e2
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-03 07:46:07 +0200

    hello

---
+++ script/myjs.js
@@ -0,0 +1,43 @@
+let num1=10;
+let num2=13;
+var avg;
+
+
+ avg=(num1+num2)/2;
+console.log(avg);
+
+
+var names =["zinhle","ntsi","smile"];
+var surname =["mahlalela","mathonsi","zwane"]
+var age=["29","13","25"];
+for(var i=0;i<1;i++)
+{
+
+console.log(names[0],surname[0],age[0]);
+console.log(names[1],surname[1],age[1]);
+console.log(names[2],surname[2],age[2]);
+
+
+}
+
+var number1=3;
+var number2=2;
+var temp;
+temp=number1
+number1=number2;
+number2=temp
+console.log(number1,number2);
+
+Math.random();
+const a=Math.floor(Math.random() * 10) + 1;
+
+console.log(a);
+
+
+function things(p1,p2)
+{
+let p12 =p1*p2;
+console.log(p12);
+
+}
+things(2,9);
